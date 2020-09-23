aae5cfd2af669e5f226aa008ee8e194408a2d493
iamvkm <vivekhere01@gmail.com>
2020-09-23 20:22:39 +0530

    modified readme

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# vtu-gpa
+# VTU-GPA
+
+![SGPA Calculator](./sgpa.png "SGPA Calculator")
+
 A simple and easy way to calculate GPA for Visvesvaraya Technological University.
--- script.js
+++ script.js
@@ -14,9 +14,4 @@ function addOne() {
   document.getElementById("my-input").innerHTML +=
     "<input type='text' class='form-control-lg' placeholder='1 Credits Subject Marks' name='1-sub' /><br /><br />";
 }
-function calculate() {
-  a = Number(document.form.num3.value);
-  b = Number(document.form.num4.value);
-  c = a + b;
-  document.form1.result1.value = c;
-}
+function calculate() {}
