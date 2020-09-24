c12227bafaf98c5c627c72b4ec3b8b4e49ea29d2
iamvkm <vivekhere01@gmail.com>
2020-09-24 13:59:28 +0530

    misc changes

--- js/cgpa.js
+++ js/cgpa.js
@@ -136,5 +136,5 @@ function calculate_cgpa() {
   cgpa = products_sum / credits_sum;
   final_cgpa = Math.round((cgpa + Number.EPSILON) * 100) / 100;
   document.getElementById("cgpa-result").innerHTML =
-    "CGPA scored by you: <span id='res-color'>" + final_cgpa + "</span>";
+    "CGPA scored by you: <span id='res-mod'>" + final_cgpa + "</span>";
 }
--- js/sgpa.js
+++ js/sgpa.js
@@ -136,5 +136,5 @@ function calculate() {
   sgpa = final_grade_credit_sum / final_credit_sum;
   final_sgpa = Math.round((sgpa + Number.EPSILON) * 100) / 100;
   document.getElementById("result").innerHTML =
-    "SGPA scored by you: <span id='res-color'>" + final_sgpa + "</span>";
+    "SGPA scored by you: <span id='res-mod'>" + final_sgpa + "</span>";
 }
